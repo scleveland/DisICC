ad833b9886420b20cd9f175b8db58cd9959066f1
Sean Cleveland <sean.b.cleveland@gmail.com>
2012-10-09 12:29:13 -0600

    adding devise and web app theme

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -3,7 +3,17 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-
+  before_filter :check_user
+  
+  private
+  
+  def check_user
+    puts "bullsiiiiit"
+    if current_user.nil?
+      redirect_to new_user_session_url
+    end
+  end
+   
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'df7bbfabe7d516b902777c5a26f31584'
--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,4 +1,14 @@
 class PageController < ApplicationController
+  before_filter :check_user
+  
+  
   def index
+  end
+  private
+  
+  def check_user
+    if current_user.nil?
+      redirect_to new_user_session_url
+    end
   end
 end
