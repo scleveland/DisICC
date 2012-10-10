cdd9a1ecadd023b154e4ce76cc9790de72e658f4
Sean Cleveland <sean.b.cleveland@gmail.com>
2012-10-10 12:52:01 -0600

    updated the ACTUAL application controller to authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 require 'dm-rails/middleware/identity_map'
 class ApplicationController < ActionController::Base
   use Rails::DataMapper::Middleware::IdentityMap
+  before_filter :authenticate_user!
   protect_from_forgery
+  
 end
