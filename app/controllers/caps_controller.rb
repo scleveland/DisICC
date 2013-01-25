aa45e077d7ba5c11ddc44bc7324e10afe962750b
Sean Cleveland <sean.b.cleveland@gmail.com>
2013-01-24 22:48:50 -0700

    updated code to use NewCaps

--- app/controllers/caps_controller.rb
+++ app/controllers/caps_controller.rb
@@ -2,7 +2,7 @@ class CapsController < ApplicationController
   # GET /caps
   # GET /caps.xml
   def index
-    @caps =  Caps.all(:fields=>[:seq_id], :unique=>true, :order=>[:seq_id])
+    @caps =  NewCaps.all(:fields=>[:seq_id], :unique=>true, :order=>[:seq_id])
     #@sequences = Sequence.all
     respond_to do |format|
       format.html # index.html.erb
